8f5fab57e1b2b8d4c908a7bbffa0976b63e092e6
IT25103932 <it25103932@my.sliit.lk>
2026-04-12 13:14:55 +0530

    default
    # Conflicts:
    #	src/main/java/com/OnlineToyStore/Sllit/model/CartItem.java
    #	src/main/java/com/OnlineToyStore/Sllit/model/Review.java
    #	src/main/java/com/OnlineToyStore/Sllit/model/Toy.java
    #	src/main/java/com/OnlineToyStore/Sllit/model/User.java

--- src/main/java/com/OnlineToyStore/Sllit/model/CartItem.java
+++ src/main/java/com/OnlineToyStore/Sllit/model/CartItem.java
@@ -11,44 +11,4 @@ public class CartItem {
     private String cartItemId;
     private String toyId;
     private int quantity;
-    private String userId;
-    private String toyName;
-    private String toyImageUrl;
-    private double unitPrice;
-
-    //encapsulation - total price calculation
-    public double getTotalPrice() {
-        return unitPrice * quantity;
-    }
-    //polymorphism-regular customer gets discount
-    public double getDiscountedPrice() {
-        return getTotalPrice();
-    }
-    // Saving   to cart.txt
-    // Format:> cartItemId|userId|toyId|toyName|toyImageUrl|unitPrice|quantity
-    public String toFileString() {
-        return cartItemId + "|" +
-                userId + "|" +
-                toyId + "|" +
-                toyName + "|" +
-                (toyImageUrl != null ? toyImageUrl : "") + "|" +
-                unitPrice + "|" +
-                quantity;
-    }
-
-    // Read from cart.txt
-    public static CartItem fromFileString(String line) {
-        String[] p = line.split("\\|", -1);
-        CartItem item = new CartItem();
-        item.setCartItemId(p[0]);
-        item.setUserId(p[1]);
-        item.setToyId(p[2]);
-        item.setToyName(p[3]);
-        item.setToyImageUrl(p.length > 4 ? p[4] : "");
-        item.setUnitPrice(Double.parseDouble(p[5]));
-        item.setQuantity(Integer.parseInt(p[6]));
-        return item;
-    }
 }
-
-
--- src/main/java/com/OnlineToyStore/Sllit/model/Toy.java
+++ src/main/java/com/OnlineToyStore/Sllit/model/Toy.java
@@ -10,35 +10,7 @@ import lombok.AllArgsConstructor;
 public class Toy {
     private String toyId;
     private String name;
-    private String category; //Educational, Electronic, Soft, General
     private String description;
     private double price;
     private int stockQuantity;
-
-    // This method will be overridden in subclasses (Polymorphism)
-    public String displayInfo(){
-        return "Toy: " + name +
-                " | Category: " + category +
-                " | Price: Rs." + price +
-                " | Stock: " + stockQuantity;
-    }
-
-    // Convert toy object to a line in toys.txt
-    public String toFileString() {
-        return toyId + "|" + name + "|" + category + "|" +
-                description + "|" + price + "|" + stockQuantity;
-    }
-
-    // Convert a line from toys.txt back to a Toy object
-    public static Toy fromFileString(String line) {
-        String[] parts = line.split("\\|");
-        Toy toy = new Toy();
-        toy.setToyId(parts[0]);
-        toy.setName(parts[1]);
-        toy.setCategory(parts[2]);
-        toy.setDescription(parts[3]);
-        toy.setPrice(Double.parseDouble(parts[4]));
-        toy.setStockQuantity(Integer.parseInt(parts[5]));
-        return toy;
-    }
 }
