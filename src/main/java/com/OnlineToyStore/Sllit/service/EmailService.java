c3c8bb3c75a12f63c6876fafd5e7731b9bc88eba
IT25103934 <it25103934@my.sliit.lk>
2026-05-22 15:12:37 +0530

    InventoryController added

--- src/main/java/com/OnlineToyStore/Sllit/service/EmailService.java
+++ src/main/java/com/OnlineToyStore/Sllit/service/EmailService.java
@@ -5,7 +5,7 @@ import com.OnlineToyStore.Sllit.model.Review;
 import com.OnlineToyStore.Sllit.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail .SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
